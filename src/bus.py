89adae68526d7dcbc642452e1831b1d329d310b0
mack millar <mackmillar@gmail.com>
2020-11-24 14:05:42 +0000

    initial commit

---
+++ src/bus.py
@@ -0,0 +1,27 @@
+class Bus:
+
+    def __init__(self, route, destination):
+        
+        self.route_number = route
+        self.destination = destination
+        self.passengers = []
+        
+
+
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, person):
+        self.passengers.remove(person)
+
+    def empty(self):
+        self.passengers.clear()
+
+    def pick_up_from_stop(self, bus_stop):
+        self.passengers.extend(bus_stop.queue)
